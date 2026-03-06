712283c757068f4c058efb43530e89dd34fcded6
Benjamin Houdu <benjamin.houdu@gmail.com>
2026-03-06 01:01:00 +0100

    :abc: WIP

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -1,26 +1,30 @@
 #!/usr/bin/env -S npx tsx
+import chalk from "chalk";
 import { Command } from "commander";
-import { runInvoke } from "./client.js";
+//import { runInvoke } from "./client.js";
 import fs from "fs";
 
 const program = new Command();
 
 program
   .name("qi")
-  .description("Postman-like CLI for Cloud Resources")
-  .requiredOption("-p, --provider <provider>", "Cloud provider (e.g., aws), default is aws")
-  .requiredOption("-e, --env <path>", "Path to env config (e.g., ./envs/uat.json)")
-  .requiredOption("-r, --request <path>", "Path to payload JSON")
+  .description("Quick Invoker of cloud resources")
+  .requiredOption("-c, --command <command>", "Command to execute like lambdaInvoke")
+  .option("-p, --profile <profile>", "Profile to use for executing the command")
+  .option("-e, --env <env>", "Path to env config (e.g., ./envs/production.json)")
+  .option("-cp, --cloud-provider <cloud_provider>", "Cloud provider (e.g., aws), default is aws")
+  .option("-r, --request <path>", "Path to payload JSON, default is no payload")
   .action(async (options) => {
-    const env = JSON.parse(fs.readFileSync(options.env, "utf-8"));
-    const payload = JSON.parse(fs.readFileSync(options.request, "utf-8"));
+		const envValue = fs.readFileSync(options.env, "utf-8");
+		const requestValue = fs.readFileSync(options.request, "utf-8");
+    const payload = JSON.parse(requestValue);
+    const env = JSON.parse(envValue);
 
     console.log(`Triggering ${chalk.blue(env.sfnName)}...`);
-    const result = await runInvoke(env.sfnArn, payload);
-
-    if (result) {
-      console.log(JSON.stringify(result, null, 2));
-    }
+    // const result = await runInvoke(env.sfnArn, payload);
+    // if (result) {
+    //   console.log(JSON.stringify(result, null, 2));
+    // }
   });
 
 program.parse();
